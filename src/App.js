93c38ce7459d3f3a76602749e6baea2d42d04f94
divami-praveen <praveen@divami.com>
2022-05-27 15:00:38 +0530

    update sid

--- src/App.js
+++ src/App.js
@@ -1,114 +1,118 @@
-import "./styles.css";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import moment from "moment";
-import TreeNode from "./components/TreeNode";
-import Tree from "./components/Tree";
-import { convertDirData, getNestorData, sid } from "./utils/utils";
-import Lottie from "react-lottie";
-import animationData from "./utils/loading-lottie";
+import './styles.css'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import moment from 'moment'
+import TreeNode from './components/TreeNode'
+import Tree from './components/Tree'
+import { convertDirData, getNestorData, sid } from './utils/utils'
+import Lottie from 'react-lottie'
+import animationData from './utils/loading-lottie'
 
-var scale = 0.75;
+var scale = 0.75
 
 export default function App() {
-  const [IsLoading, setIsLoading] = useState("loading");
-  const [empDirState, setEmpDirState] = useState([]);
-  const [empDirFullState, setEmpDirFullState] = useState([]);
+  const [IsLoading, setIsLoading] = useState('loading')
+  const [empDirState, setEmpDirState] = useState([])
+  const [empDirFullState, setEmpDirFullState] = useState([])
 
   const scaleCanvas = (numberOfChildren) => {
-    let scaleFactor = scale;
-    let lengthOfChildren = (numberOfChildren + 1) * 183;
-    let windowWidth = window.innerWidth;
+    let scaleFactor = scale
+    let lengthOfChildren = (numberOfChildren + 1) * 183
+    let windowWidth = window.innerWidth
 
-    scaleFactor = Number(Number(windowWidth / lengthOfChildren).toFixed(2));
+    scaleFactor = Number(Number(windowWidth / lengthOfChildren).toFixed(2))
     // console.log("scaleFactor", scaleFactor, lengthOfChildren, windowWidth)
     if (scaleFactor > scale) {
-      scaleFactor = scale;
+      scaleFactor = scale
     }
     document.getElementById(
-      "AppCanvas"
-    ).style.transform = `scale(${scaleFactor})`;
-  };
+      'AppCanvas'
+    ).style.transform = `scale(${scaleFactor})`
+  }
 
   const getOrgData = async () => {
     try {
-      const res = await axios.post("https://nestor.divami.com/request.php", {
-        command: "getEmployeeListView",
+      const res = await axios.post('https://nestor.divami.com/request.php', {
+        command: 'getEmployeeListView',
         client: { sid: sid },
-        data: { status: "Active" }
-      });
-      const empDir = Array.from(res?.data?.data);
-      let convertedDir = convertDirData(empDir);
+        data: { status: 'Active' },
+      })
+      const empDir = Array.from(res?.data?.data)
+      let convertedDir = convertDirData(empDir)
       convertedDir = convertedDir.map((x) => ({
         ...x,
         id: `tree-node-${x.userId}`,
         numberOfChildren: convertedDir.filter(
           (y) => y.userReportingTo === x.userFirstName
-        ).length
-      }));
+        ).length,
+      }))
       // const root = convertedDir.find((x) => x.userReportingTo === null);
       setEmpDirState((_prev) => [
         { ...convertedDir[0] },
         ...convertedDir.filter(
           (x) => x.userReportingTo === convertedDir[0].userFirstName
-        )
-      ]);
-      setEmpDirFullState(convertedDir);
+        ),
+      ])
+      setEmpDirFullState(convertedDir)
     } catch (err) {
-      setIsLoading("API Call Failed");
+      setIsLoading('API Call Failed')
     }
-  };
+  }
 
   useEffect(() => {
-    getOrgData().catch();
-    scaleCanvas(scale);
-  }, []);
+    getOrgData().catch()
+    scaleCanvas(scale)
+  }, [])
   useEffect(() => {
-    if (IsLoading === "loading" && document.getElementById("loader")) {
-      document.getElementById("loader").scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-        inline: "center"
-      });
+    if (IsLoading === 'loading' && document.getElementById('loader')) {
+      document.getElementById('loader').scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center',
+      })
     }
-    if (IsLoading !== "loading" && document.getElementById("App")) {
-      document.getElementById("App").scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-        inline: "center"
-      });
+    if (IsLoading !== 'loading' && document.getElementById('App')) {
+      document.getElementById('App').scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center',
+      })
     }
-  });
+    var list = document.getElementById('root').parentElement.children
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].localName === 'iframe') list[i].remove()
+    }
+  })
 
   const genParent = (CurrEmp, Parent) => {
     if (CurrEmp.userReportingTo !== null) {
       const CurrEmpParent = empDirFullState.find(
         (x) => x.userFirstName === CurrEmp.userReportingTo
-      );
-      return genParent(CurrEmpParent, [CurrEmp, ...Parent]);
-    } else return [CurrEmp, ...Parent];
-  };
+      )
+      return genParent(CurrEmpParent, [CurrEmp, ...Parent])
+    } else return [CurrEmp, ...Parent]
+  }
 
   const onNodeClick = (event, emp) => {
     setTimeout(() => {
       document.getElementById(emp.id).scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-        inline: "start"
-      });
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'start',
+      })
       scaleCanvas(
         empDirFullState.filter((x) => x.userReportingTo === emp.userFirstName)
           .length
-      );
-    }, 500);
-    event.stopPropagation();
-    let parents = [];
-    parents = genParent(emp, parents);
+      )
+    }, 500)
+    event.stopPropagation()
+    let parents = []
+    parents = genParent(emp, parents)
     const children = empDirFullState.filter(
       (x) => x.userReportingTo === emp.userFirstName
-    );
-    setEmpDirState((_prev) => [...parents, ...children]);
-  };
+    )
+    setEmpDirState((_prev) => [...parents, ...children])
+  }
 
   const genTreeNode = (parentName) => {
     return empDirState
@@ -117,17 +121,17 @@ export default function App() {
         <TreeNode emp={emp} onClick={(e) => onNodeClick(e, emp)}>
           {genTreeNode(emp.userFirstName)}
         </TreeNode>
-      ));
-  };
+      ))
+  }
 
   return (
     <div className="AppCanvas" id="AppCanvas">
       <div className="App" id="App">
         {empDirState.length > 0 ? (
           <Tree
-            lineWidth={"3px"}
-            lineColor={"#ccc"}
-            lineBorderRadius={"50px"}
+            lineWidth={'3px'}
+            lineColor={'#ccc'}
+            lineBorderRadius={'50px'}
             emp={empDirState[0]}
             onClick={(e) => onNodeClick(e, empDirState[0])}
           >
@@ -135,7 +139,7 @@ export default function App() {
           </Tree>
         ) : (
           <div className="loader" id="loader">
-            {IsLoading === "loading" ? (
+            {IsLoading === 'loading' ? (
               <div>
                 <Lottie
                   options={{
@@ -143,8 +147,8 @@ export default function App() {
                     autoplay: true,
                     animationData: animationData,
                     rendererSettings: {
-                      preserveAspectRatio: "xMidYMid slice"
-                    }
+                      preserveAspectRatio: 'xMidYMid slice',
+                    },
                   }}
                   height={400}
                   width={500}
@@ -162,5 +166,5 @@ export default function App() {
         )}
       </div>
     </div>
-  );
+  )
 }
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,43 +1,43 @@
-import moment from "moment";
-import axios from "axios";
+import moment from 'moment'
+import axios from 'axios'
 
-export const sid = "h700c2qthusq2itics4covhgs1";
+export const sid = 'acelnugc8tdhthuc06p3nnvnv5'
 
 export const getNestorData = axios.post(
-  "https://nestor.divami.com/request.php",
+  'https://nestor.divami.com/request.php',
   {
-    command: "getEmployeeListView",
+    command: 'getEmployeeListView',
     client: { sid },
-    data: { status: "Active" }
+    data: { status: 'Active' },
   }
-);
+)
 
 const checkImg = async (url: string) => {
   try {
-    const res = await axios.get(url);
+    const res = await axios.get(url)
     if (res.status === 200) {
-      return url;
+      return url
     }
-    return false;
+    return false
   } catch (_err) {
-    return false;
+    return false
   }
-};
+}
 
 export const convertDirData = (empDir: any) => {
-  return empDir.map((emp: { employeeId: any; firstname: string; lastname: string; profilepic: string; designations: string | any[]; startdate: moment.MomentInput; }) => ({
+  return empDir.map((emp: any) => ({
     userId: emp.employeeId,
     userFirstName: emp.firstname,
-    userName: emp.firstname + " " + emp.lastname,
-    userPhoto: "https://nestor.divami.com" + emp.profilepic, // checkImg("https://nestor.divami.com" + emp.profilepic),
+    userName: emp.firstname + ' ' + emp.lastname,
+    userPhoto: 'https://nestor.divami.com' + emp.profilepic, // checkImg("https://nestor.divami.com" + emp.profilepic),
     userDesignation:
       emp.designations && emp.designations.length
         ? emp.designations[emp.designations.length - 1].designation
-        : "Unassigned",
-    userJoinedOn: moment(emp.startdate, "YYYY-MM-DD").format("DD MMM, YYYY"),
+        : 'Unassigned',
+    userJoinedOn: moment(emp.startdate, 'YYYY-MM-DD').format('DD MMM, YYYY'),
     userReportingTo:
       emp.designations && emp.designations.length
         ? emp.designations[emp.designations.length - 1].reportingTo
-        : "null"
-  }));
-};
+        : 'null',
+  }))
+}
